c785ab1677c82d62a60727c69730d7b97bdd93a2
Antonin Stoklasek <antonin.stoklasek@gmail.com>
2020-08-01 20:48:04 +0200

    additional commit

--- src/main/java/net/osomahe/coinacrobat/api/Currency.java
+++ src/main/java/net/osomahe/coinacrobat/api/Currency.java
@@ -2,6 +2,10 @@ package net.osomahe.coinacrobat.api;
 
 public class Currency implements Catalogable {
 
+    public static Currency of(String code) {
+        return null;
+    }
+
     @Override
     public Catalog getType() {
         return Catalog.CURRENCY;
